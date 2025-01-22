57ea23aad40bc0d751d421b939cd52da1cfd5b60
Khaled BENKADOUR <benkkadour.khaled@gmail.com>
2025-01-22 11:05:58 +0100

    Comparator Interface

--- src/main/java/dz/benkadour/khaled/lambdas/ComparatorLambdaExample.java
+++ src/main/java/dz/benkadour/khaled/lambdas/ComparatorLambdaExample.java
@@ -1,9 +1,31 @@
 package dz.benkadour.khaled.lambdas;
-
+import java.util.Comparator;
 public class ComparatorLambdaExample {
 
     public static void main(String[] args) {
 
+        /*
+        * Before Java 8
+        * */
+
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+
+        };
+
+        System.out.println("Result of the comparator is : " + comparator.compare(3, 2));
+
+        /*
+        * Java 8 and After
+        * */
+
+        Comparator<Integer> comparator2 = (Integer a, Integer b) -> a.compareTo(b);;
+
+        System.out.println("Result of the comparator using Lambda is : " + comparator2.compare(3, 2));
     }
 
 }
