aa43df790fbfc7a6c07b065a2013806445cb7000
Khaled BENKADOUR <benkkadour.khaled@gmail.com>
2025-02-04 09:02:53 +0100

    reduce() operation

---
+++ src/main/java/dz/benkadour/khaled/streams/StreamsReduceExamples.java
@@ -0,0 +1,21 @@
+package dz.benkadour.khaled.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsReduceExamples {
+
+    public static int performMultiplication(List<Integer> integerList) {
+        return integerList
+                .stream()
+                .reduce(1, (a, b) -> (a * b));
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> integers = Arrays.asList(1, 3, 5, 7);
+        System.out.println(performMultiplication(integers));
+
+    }
+
+}
