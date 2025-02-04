5248dc8a38c13517f54736c4a0c1b5c63d83b368
Khaled BENKADOUR <benkkadour.khaled@gmail.com>
2025-02-04 10:05:25 +0100

    reduce() operation without identity of start

--- src/main/java/dz/benkadour/khaled/streams/StreamsReduceExamples.java
+++ src/main/java/dz/benkadour/khaled/streams/StreamsReduceExamples.java
@@ -2,6 +2,7 @@ package dz.benkadour.khaled.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamsReduceExamples {
 
@@ -11,11 +12,20 @@ public class StreamsReduceExamples {
                 .reduce(1, (a, b) -> (a * b));
     }
 
+    public static Optional<Integer> performMultiplicationWithoutIdentity(List<Integer> integerList) {
+        return integerList
+                .stream()
+                .reduce((a, b) -> (a * b));
+    }
+
     public static void main(String[] args) {
 
         List<Integer> integers = Arrays.asList(1, 3, 5, 7);
         System.out.println(performMultiplication(integers));
-
+        System.out.println("***************************");
+        Optional<Integer> result = performMultiplicationWithoutIdentity(integers);
+        System.out.println(result.isPresent());
+        System.out.println(result.get());
     }
 
 }
